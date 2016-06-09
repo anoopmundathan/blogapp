888afa4f4b56a65580fe9cbd3d49796f62a360d6
anoopmundathan <anoop.mundathan@gmail.com>
2016-06-09 10:58:05 +0100

    Add some comment

--- server.js
+++ server.js
@@ -1,20 +1,29 @@
 var express = require('express');
 var mongoose = require('mongoose');
 
+// Connect to mongodb
 mongoose.connect('mongodb://localhost/blogroll');
+
+// Create Schema
 var Schema = mongoose.Schema;
+
 var BlogSchema = new Schema({
 	author: String,
 	title: String,
 	url: String
 });
+
 mongoose.model('Blog',BlogSchema);
+
 var Blog = mongoose.model('Blog');
+
 var blog = new Blog({
 	author: 'Anoop',
 	title: 'Mundathan',
 	url: 'www.abc.com'
 });
+
+// Save the model
 blog.save();
 
 var app = express();
